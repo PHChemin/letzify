62f937a9994dc849f61474b42a21b6ede348d101
Pedro Henrique Chemin <134353389+PHChemin@users.noreply.github.com>
2026-04-25 18:23:19 -0300

    Setup Domain Modules (#17)
    * Fix dependencies error in IDE
    
    * Project infrastructure based on sdd

--- apps/api/src/app.module.ts
+++ apps/api/src/app.module.ts
@@ -1,10 +1,18 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthModule } from './auth/auth.module';
+import { UsersModule } from './users/users.module';
+import { ProjectsModule } from './projects/projects.module';
+import { PalettesModule } from './palettes/palettes.module';
+import { TypographiesModule } from './typographies/typographies.module';
+import { AssetsModule } from './assets/assets.module';
+import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from './config/config.module';
 
 @Module({
-  imports: [],
+  imports: [AuthModule, UsersModule, ProjectsModule, PalettesModule, TypographiesModule, AssetsModule, PrismaModule, ConfigModule],
   controllers: [AppController],
   providers: [AppService],
 })
-export class AppModule {}
+export class AppModule { }
